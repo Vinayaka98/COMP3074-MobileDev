42f6b9d237117fe2e407652dd326863d07becd3a
Vinayaka98 <polimeravinayaka@gmail.com>
2022-09-25 19:27:03 -0400

    Successfully Added

--- Labs/Lab3/app/src/main/java/com/georgebrown/lab3/MainActivity.java
+++ Labs/Lab3/app/src/main/java/com/georgebrown/lab3/MainActivity.java
@@ -1,17 +1,19 @@
 package com.georgebrown.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button activity_btn, web_btn, map_btn, menu;
+    private Button activity_btn, web_btn, map_btn;
 
 
     @Override
@@ -21,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         activity_btn = findViewById(R.id.activity_btn);
         web_btn = findViewById(R.id.web_btn);
         map_btn = findViewById(R.id.map_btn);
-        menu = findViewById(R.id.menu);
 
         activity_btn.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(),
@@ -37,13 +38,31 @@ public class MainActivity extends AppCompatActivity {
 
         map_btn.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("geo:43.6761356, -79.4130149?q="+Uri.encode("restaurants")));
+            intent.setData(Uri.parse("geo:43.6761356, -79.4130149?q=" + Uri.encode("restaurants")));
             startActivity(intent);
         });
 
-        onOptionsItemSelected(Menu men)
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id){
+            case R.id.activity_item:
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+                return true;
+            default:return super.onOptionsItemSelected(item);
         }
+
 
     }
 }
